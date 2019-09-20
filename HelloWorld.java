1f1836bcf5294143a7c1d5477eb2e0b91a94b9c1
jannatulafiqah <55247821+jannatulafiqah@users.noreply.github.com>
2019-09-20 15:22:34 +0800

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld{
+
+public static void main(String[]args){
+
+  
+  
+  int a = 3*6*1/2;
+  int b = 3*4*1/2;
+  int c = 3*4;
+  int total = a+b+c;
+  
+  System.out.print("a = ");
+  System.out.println (a);
+  System.out.print("b = ");
+  System.out.println(b);
+  System.out.print("c = ");
+  System.out.println(c);
+  System.out.print("total = ");
+  System.out.println(total);
+  
+  }
+  }
